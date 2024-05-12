10ecc190751b9889ba09ca2b948b8fa7728e8ff1
nyakako <nyakako13@gmail.com>
2024-05-12 10:53:24 +0900

    feat: #2 - 実装完了

---
+++ src/__tests__/AppComponent.test.tsx
@@ -0,0 +1,9 @@
+﻿import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("title", () => {
+	it("should render title", () => {
+		render(<App />);
+		expect(screen.getByText("Hello World")).toBeInTheDocument();
+	});
+});
