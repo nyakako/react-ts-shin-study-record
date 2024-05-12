f2a8cd8dd46f2359b25cad6a4bc42e10e0873c47
nyakako <nyakako13@gmail.com>
2024-05-13 02:26:34 +0900

    feat: #2 ChakraUI,supabase設定完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
+import {
+	Spinner,
+	Table,
+	TableContainer,
+	Tbody,
+	Td,
+	Th,
+	Thead,
+	Tr,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { StudyRecord } from "./domain/studyRecord";
+import fetchStudyRecords from "./utils/supabaseFunctions";
+
 function App() {
+	const [records, setRecords] = useState<StudyRecord[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		const getAllStudyRecords = async () => {
+			const studyRecordData = await fetchStudyRecords();
+			console.log(studyRecordData);
+			setRecords(studyRecordData);
+			setIsLoading(false);
+		};
+		getAllStudyRecords();
+	});
 	return (
 		<>
-			<h1 data-testid="title">Hello World 5/12</h1>
+			<h1 data-testid="title">Shin Study Record</h1>
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<TableContainer>
+					<Table variant="striped" colorScheme="blackAlpha">
+						<Thead>
+							<Tr>
+								<Th>title</Th>
+								<Th>time</Th>
+								<Th>created_at</Th>
+							</Tr>
+						</Thead>
+						<Tbody>
+							{records.map((record) => (
+								<Tr key={record.id}>
+									<Td>{record.title}</Td>
+									<Td>{record.time}</Td>
+									<Td>{record.created_at}</Td>
+								</Tr>
+							))}
+						</Tbody>
+					</Table>
+				</TableContainer>
+			)}
 		</>
 	);
 }
---
+++ src/utils/supabaseFunctions.ts
@@ -0,0 +1,20 @@
+﻿import { StudyRecord } from "../domain/studyRecord";
+import { supabase } from "./supabase";
+
+export default async function fetchStudyRecords() {
+	const response = await supabase.from("study-record").select("*");
+
+	if (response.error) {
+		throw new Error(response.error.message);
+	}
+
+	const studyRecordData = response.data.map((record) => {
+		return new StudyRecord(
+			record.id,
+			record.title,
+			record.time,
+			record.created_at
+		);
+	});
+	return studyRecordData;
+}
